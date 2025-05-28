021672c6b8916cf0d7e2be1ade041bcd2207364d
Ting <minting063@gmail.com>
2025-05-28 18:53:36 +0800

    added hair clip

---
+++ minting/Item_data.py
@@ -0,0 +1,13 @@
+
+
+clothing_DATABASE = {
+    "FishBone_Clip": {
+        "type": "accessory",
+        "slot": "hair",
+        "image": "minting/fishbone.png",
+        "icon": "minting/FishBoneClip.png",
+        "name": "Fish Bone Hair Clip",
+        "description": "A stylish clip. Might smell fishy."
+    },
+    # Add more items here...
+}  
